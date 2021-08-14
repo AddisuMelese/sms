d9121f6704dd16758ceafc344490840fa8a0acfa
AddisuMelese <86000440+AddisuMelese@users.noreply.github.com>
2021-08-14 17:39:11 +0300

    change

--- students/index.php
+++ students/index.php
@@ -2,7 +2,7 @@
 $title="Students Home"; 
     require_once "includes/header.php"; 
 ?>
-    <h1>School Manegment System! students</h1>
+    <h1>School Manegment System! student</h1>
 <?php 
    require_once "includes/footer.php"; 
 ?>
